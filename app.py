25b879bda5cc7fed344ed94b7741fd28d617aa5a
Shubham Aware <sandyaware07@gmail.com>
2023-11-29 23:51:31 +0530

    Commiting Bsic Streamlit structure code for web interface

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title='Chat With Multiple PDFs',page_icon=':books:')
+
+    st.header('Chat with multiple PDFs :books:')
+    st.text_input('Ask a question about your documents:')
+
+    with st.sidebar:
+        st.subheader("Your Document's")
+        st.file_uploader("Upload your PDFs here and click on Process")
+        st.button("Process")
+
+if __name__ == '__main__':
+    main()
