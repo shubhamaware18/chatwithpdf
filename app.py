0417287358b12c2ec4da78af9ce837d17ce10706
Shubham Aware <sandyaware07@gmail.com>
2023-11-30 21:33:08 +0530

    commiting updated readme file

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+from dotenv import load_dotenv
+
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title='Chat With Multiple PDFs',page_icon=':books:')
 
     st.header('Chat with multiple PDFs :books:')
